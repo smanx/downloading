0ac2ebda94e0723d0a13b657ba750b64ac0c840a
zc <zc@zc.com>
2024-04-27 00:11:20 +0800

    save

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,12 @@
-#!/bin/sh  
-  
-# Set the download URL using the environment variable DOWNLOAD_URL  
-DOWNLOAD_URL=${DOWNLOAD_URL:-https://example.com/somefile}  
-  
-# Initialize the loop counter  
-COUNTER=1  
-  
-while true; do  
-    # Print the loop count and the download URL before starting the download  
-    echo "Loop Count: $COUNTER - Starting download: $DOWNLOAD_URL"  
-    wget -q "$DOWNLOAD_URL" -O /dev/null  
-      
-    # Increment the counter for the next loop  
+#!/bin/sh
+# Set the download URL using the environment variable DOWNLOAD_URL
+DOWNLOAD_URL=${DOWNLOAD_URL:-https://test.ustc.edu.cn/backend/garbage.php?ckSize=100}
+# Initialize the loop counter
+COUNTER=1
+while true; do
+    # Print the loop count and the download URL before starting the download
+    echo "Loop Count: $COUNTER - Starting download: $DOWNLOAD_URL"
+    wget -q "$DOWNLOAD_URL" -O /dev/null
+    # Increment the counter for the next loop
     COUNTER=$((COUNTER + 1))
 done
